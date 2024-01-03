490dcdeef5c3188f6fbace7a7558618d48b5a9ad
Omar Gutierrez Sanchez <117556558+spirosis@users.noreply.github.com>
2024-01-03 05:08:34 +0000

    first

--- src/models.py
+++ src/models.py
@@ -7,23 +7,63 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    
+    user_id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique = True)
+    email = Column(String(250), nullable = False, unique = True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+   
+class Planets(Base):
+    __tablename__ = 'planets'
+
+    planet_id = Column(Integer, primary_key = True )
+    name = Column(String(50), unique = True)
+    diameter = Column(Integer, nullable = True)
+    mass = Column(Integer, nullable = True)
+    population = Column(Integer, nullable = True)
+    rotation_period = Column(Integer, nullable = False)
+    climate = Column(String(50), nullable = False)
+    terrain = Column(String(50), nullable = False)
+    orbital_period = Column(Integer, nullable = False)
+    character = relationship('Character')
+
+class Starship(Base):
+
+    __tablename__ = 'starship'
+
+    starship_id = Column(Integer, primary_key = True )
+    name = Column(String(50), unique = True)
+    starship_id = Column(Integer, ForeignKey('starship.id'))
+    model = Column(String(25), nullable = False)
+    manufacture = Column(String(25), nullable = False)
+    cost_in_credits = Column(Integer, nullable = False)
+    created = Column(String(50), nullable = False)
+    crew = Column(Integer, nullable = False)
+    edited = Column(String(25), nullable = False)
+    hyperdrive_rating = Column(Integer, nullable = False)
+    consumables = Column(Integer, (25), nullable = False)
+    cargo_capacity = Column(Integer, nullable = False)
+    starship_class = Column(String(50))
+    description = Column(String(250))
+    pilots = Column(Integer, nullable = False)
+    character = relationship('Character')
+
+class Character(Base):
+    __tablename__ = 'character'
+
+    character_id = Column(Integer, primary_key = True )
+    name = Column(String(50), unique = True)
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+
+    character_id = Column(Integer, ForeignKey('character.character_id') nullable = True)
+    planet_id = Column(Integer, ForeignKey('planets.planet_id'), nullable = True)
+    starship_id = Column(Integer, ForeignKey('starship.starship_id'), nullable = True)
+    user_id = Column(Integer, ForeignKey('user.user_id'), nullable = True)
 
     def to_dict(self):
         return {}
